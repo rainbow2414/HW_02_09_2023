1d8b2cc99962b12efed6b60e2228055099ec1b40
rainbow2414 <rainbow@ngs.ru>
2023-09-21 22:04:50 +0700

    Tasks 64,66,68

---
+++ Task_66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Sum(int M, int N)
+{
+    if (M == N) return M;
+    return M + Sum(M + 1, N);
+}
+
+int first = InputNum("введите число M: ");
+int second = InputNum("введите число N: ");
+int sum = Sum(first, second);
+Console.WriteLine($"Сумма чисел от {first} до {second} равна {sum}");
---
+++ Task_68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int Ackerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0) return Ackerman(M - 1, 1);
+    return Ackerman(M - 1, Ackerman(M, N - 1));
+}
+
+int first = InputNum("введите число M: ");
+int second = InputNum("введите число N: ");
+int ack = Ackerman(first, second);
+Console.WriteLine($"Функция Аккермана для чисел {first} и {second} равна {ack}");
+
